0e3b75e9f6a02b314dc46618dc6738d7d0a0882a
Alex Jercan <jercan_alex27@yahoo.com>
2023-08-04 11:13:11 +0300

    small fix to accomodate my change in codehint

--- nvim/after/plugin/codehint.lua
+++ nvim/after/plugin/codehint.lua
@@ -1,11 +1,11 @@
 local codehint = require("codehint")
 
 codehint.setup({
-    api = {
+    provider = "openai",
+    args = {
+        use_env = false,
         model = "gpt-3.5-turbo",
-        endpoint = "https://api.openai.com/v1/chat/completions",
     },
-    use_env = false,
 })
 
 vim.keymap.set("n", "<leader>h", codehint.hint)
