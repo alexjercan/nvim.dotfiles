0c1d137f71cdc421908e5429bc141c8af8d7d954
Alex Jercan <jercan_alex27@yahoo.com>
2024-03-16 18:59:39 +0200

    dk

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,16 +1,20 @@
 function ColorMyAustere()
+    vim.opt.background = "dark"
+    vim.opt.termguicolors = true
     vim.cmd.colorscheme("austere")
 
     vim.api.nvim_set_hl(0, "Constant", { fg = "#ce5252" })
-	vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#252525" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#252525" })
 end
 
 function ColorMyBreakingBad()
     vim.opt.background = "dark"
     vim.opt.termguicolors = true
-	vim.cmd.colorscheme("breakingbad")
+    vim.cmd.colorscheme("breakingbad")
 
-	vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#404040" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#404040" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 function ColorMyGruvbox()
@@ -18,15 +22,25 @@ function ColorMyGruvbox()
     vim.opt.termguicolors = true
     vim.cmd.colorscheme("gruvbox")
 
-    vim.cmd("hi Normal guibg=None")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+function ColorMyRosePine()
+    vim.opt.background = "dark"
+    vim.opt.termguicolors = true
+    vim.cmd.colorscheme("rose-pine")
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    { "rose-pine/neovim", name = "rose-pine" },
-    { "morhetz/gruvbox", name = "gruvbox", config = ColorMyGruvbox },
+    { "rose-pine/neovim",      name = "rose-pine" },
+    { "morhetz/gruvbox",       name = "gruvbox",   config = ColorMyGruvbox },
     { "folke/tokyonight.nvim", name = "tokyonight" },
-    { "w0ng/vim-hybrid", name = "hybrid" },
-    "i3d/vim-jimbothemes" ,
+    { "w0ng/vim-hybrid",       name = "hybrid" },
+    "i3d/vim-jimbothemes",
     "chrisbra/Colorizer",
     "lurst/austere.vim",
 }
