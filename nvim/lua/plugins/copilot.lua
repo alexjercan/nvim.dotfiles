e949fb6c419ede35c992d614471d18af035c03f9
Alex Jercan <jercan_alex27@yahoo.com>
2024-04-07 20:35:22 +0300

    feat: no copilot user sighted, gigachad detected

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,7 +1,8 @@
 return {
     "github/copilot.vim",
     config = function()
+        vim.g.copilot_enabled = 0;
         vim.g.copilot_filetypes = {
-        }
+        };
     end
 }
---
+++ nvim/lua/plugins/zenmode.lua
@@ -0,0 +1,17 @@
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").setup {
+                window = {
+                    width = 90,
+                    options = { }
+                },
+            }
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = true
+            vim.wo.rnu = true
+        end)
+    end
+}
