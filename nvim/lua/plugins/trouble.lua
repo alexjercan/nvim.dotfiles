69c0469bcd5595cdbc1636e2a95c6329840ca811
Alex Jercan <jercan_alex27@yahoo.com>
2024-06-15 09:56:25 +0300

    update

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,14 +2,14 @@ return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        local trouble = require("trouble.providers.telescope")
+        local trouble = require("trouble.sources.telescope")
         local telescope = require("telescope")
 
         telescope.setup({
             defaults = {
                 mappings = {
-                    i = { ["<c-t>"] = trouble.open_with_trouble },
-                    n = { ["<c-t>"] = trouble.open_with_trouble },
+                    i = { ["<c-t>"] = trouble.open },
+                    n = { ["<c-t>"] = trouble.open },
                 },
             },
         })
